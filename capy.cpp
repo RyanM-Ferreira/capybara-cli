781aad69de3bfb59a02aeac5991be6de0bfda63b
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-06-27 19:11:11 -0300

    Capybara

---
+++ capy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << R"(
+
+    /\__/\_
+   /       \
+  /    0    \----------_
+ | Y                    \  Capybara Supremacy!
+ \______/                \
+       \                 |
+        |   /-______-|    \
+        |   |         \   /
+       //__/         //__/
+
+    )" << endl;
+    return 0;
+}
