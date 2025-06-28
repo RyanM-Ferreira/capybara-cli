c79a71819e5585e716fe3dff7e7012062c68bc57
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-06-28 14:03:49 -0300

    Agora argumentos grandes não quebram a capivara

--- capy.cpp
+++ capy.cpp
@@ -1,35 +1,60 @@
 #include <iostream>
-#include <format>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+    string input;
 
-    std::string input;
-
-    if (argc > 1) {
+    if (argc > 1)
+    {
         input = argv[1];
     }
-    else {
+    else
+    {
         input = "Capybara Supremacy!";
     }
 
-    std::string capybara;
-        
-    capybara = std::format(R"(
+    string capybara[] = {
+        "    /\\__/\\_",
+        "   /       \\",
+        "  /    0    \\----------_",
+        " | Y                    \\",
+        " \\______/                \\",
+        "       \\                 |",
+        "        |   /-______-|    \\",
+        "        |   |         \\   /",
+        "       //__/         //__/"};
 
-    /\__/\_
-   /       \
-  /    0    \----------_
- | Y                    \  {}
- \______/                \
-       \                 |
-        |   /-______-|    \
-        |   |         \   /
-       //__/         //__/
+    // ? Talvez organizar isso aqui? Tá meio tenso de se entender.
+    const int capybaraLines = sizeof(capybara) / sizeof(capybara[0]);
+    const int maxPerLine = 19;
+    int textStartLine = 2;
 
-    )",input);
+    size_t biggestLine = 0;
+    for (const auto line : capybara)
+    {
+        if (line.length() > biggestLine)
+        {
+            biggestLine = line.length();
+        }
+    }
 
-    cout << capybara << endl;
+    for (int i = 0; i < capybaraLines; i++)
+    {
+        cout << capybara[i];
+
+        int textIndex = i - textStartLine;
+        int startChar = textIndex * maxPerLine;
+
+        if (textIndex >= 0 && startChar < input.length())
+        {
+            string preText(biggestLine - capybara[i].length() + 2, ' ');
+
+            cout << preText << input.substr(startChar, maxPerLine);
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
