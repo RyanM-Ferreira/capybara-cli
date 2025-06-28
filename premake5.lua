45f85e844a291676102bce82a3698fff089ce8c7
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-06-28 14:08:30 -0300

    v0.3 adjust

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,21 @@
 workspace "capybara"
-    configurations { "Debug", "Release" }
+configurations {"Debug", "Release"}
+architecture "x86_64"
+system "windows"
 
 project "capybara-cli"
-    kind "ConsoleApp"
-    language "c++"
-    cppdialect "C++20"
-    targetdir "bin/%{cfg.system}_%{cfg.architecture}/%{cfg.buildcfg}"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++20"
+targetdir "bin/%{cfg.system}_%{cfg.architecture}/%{cfg.buildcfg}"
 
-    files {"**.cpp" }
+files {"**.cpp"}
 
-    filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
+filter "configurations:Debug"
+defines {"DEBUG"}
+symbols "On"
 
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+filter "configurations:Release"
+defines {"NDEBUG"}
+optimize "On"
+targetname "capysay"
